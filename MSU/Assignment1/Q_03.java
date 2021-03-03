eb1664cd003ce35e990433cffd19ae002334a70d
Amsyar Malek <muhammad.amsyar@mytukar.com>
2021-03-04 02:27:50 +0800

    update some script

--- MSU/Assignment1/Q_03.java
+++ MSU/Assignment1/Q_03.java
@@ -1,4 +1,4 @@
-package MSU.Assignment1;
+package MSU.Assignment1; // set current directory
 
 import java.util.concurrent.TimeUnit;
 import MSU.Assignment1.classes.BMI;
@@ -8,35 +8,50 @@ public class Q_03
 {
     public static void main(String[] args) throws InterruptedException 
     {
-        // Call BMI class -- uses nested if statement
-        BMI first = new BMI();
-        BMI_SwitchCase second = new BMI_SwitchCase();
+        // call BMI class -- uses nested if statement.
+        BMI first = new BMI(); // assign first to BMI class as the new operator instantiates the BMI method
+        System.out.print("\033[H\033[2J"); // moves the cursor at the top left corner of the screen or console and clears the screen from the cursor to the end of the screen.
+        System.out.flush(); // flush out the previous console screen.
+        System.out.println("[OOP ASSIGNMENT 1]: QUESTION 3 - Nested If Statement\n\nWelcome my friend..\nThis cheap program will calculate your BMI."); // print out the main display for Nested If Statement.
+        TimeUnit.SECONDS.sleep(4); // set the console to sleep for 4 seconds.
+        first.ifNested(); // invoke variable of first and execute the ifNested method from BMI class.
+        TimeUnit.SECONDS.sleep(3); // set the console to sleep for 3 seconds.
 
-        System.out.print("\033[H\033[2J");
-        System.out.flush(); 
-        System.out.println("Nested If Statement");
-        TimeUnit.SECONDS.sleep(2);  
-        first.ifNested();
-        TimeUnit.SECONDS.sleep(3);
-
-        System.out.print("\033[H\033[2J");
-        System.out.flush(); 
-        System.out.println("Switch Case Statement");
-        TimeUnit.SECONDS.sleep(2);
-        second.switchCase();
-        TimeUnit.SECONDS.sleep(3);
+        // call BMI class -- uses switch case statement.
+        BMI_SwitchCase second = new BMI_SwitchCase(); // assign second to BMI_SwitchCase class as the new operator instantiates the BMI_SwitchCase method
+        System.out.print("\033[H\033[2J"); // moves the cursor at the top left corner of the screen or console and clears the screen from the cursor to the end of the screen.
+        System.out.flush(); // flush out the previous console screen.
+        System.out.println("[OOP ASSIGNMENT 1]: QUESTION 3 - Switch Case Statement\n\nWelcome my friend..\nThis cheap program will calculate your BMI."); // print out the main display for Switch Case Statement.
+        TimeUnit.SECONDS.sleep(4); // set the console to sleep for 4 seconds.
+        second.switchCase(); // invoke variable of second and execute the switchCase method from BMI_SwitchCase class.
+        TimeUnit.SECONDS.sleep(3); // set the console to sleep for 3 seconds.
+    }
+}
 
 
 
-        // TimeUnit.SECONDS.sleep(3);
 
-        // System.out.print("\033[H\033[2J");
-        // System.out.flush(); 
 
-        // System.out.println("Switch Case Statement");
-        // TimeUnit.SECONDS.sleep(6);
-        // // Call BMI_SwitchCase class -- uses switch case function
-        // BMI_SwitchCase.main(ifNested);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -84,14 +99,6 @@ public class Q_03
     //         executeProg = true;
     //     }
 
-
-
-
-
-
-
-
-
         // System.out.println("[OOP ASSIGNMENT 1]: QUESTION 3\n\nWelcome my friend..\nThis cheap program will calculate your BMI.");
         // TimeUnit.SECONDS.sleep(6);
         // // Call BMI_SwitchCase class -- uses switch case function
@@ -120,10 +127,4 @@ public class Q_03
         //         BMI_SwitchCase.main(args);
         //         TimeUnit.SECONDS.sleep(3);
         //     }
-        // }        
-
-
-
-    }
-}
-
+        // } 
