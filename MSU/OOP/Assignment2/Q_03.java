547eae7f5bb9f20a5ea070c80a40e0f634ac73b8
Amsyar Malek <muhammad.amsyar@mytukar.com>
2021-03-20 01:52:13 +0800

    Update a new script!

--- MSU/OOP/Assignment2/Q_03.java
+++ MSU/OOP/Assignment2/Q_03.java
@@ -1,5 +1,11 @@
-package MSU.OOP.Assignment2;
+package MSU.OOP.Assignment2; // Set current working directory.
+import MSU.OOP.Assignment2.classes.enoNumber; // Import enoNumber class from local directory.
 
-public class Q_03 {
-    
+public class Q_03 // Construct java main class for OOP A2 Question 3
+{
+    public static void main(String[] args) // Compile and execute the java program.        
+    {
+        enoNumber mainENO = new enoNumber(); // Assign mainENO as a enoNumber class variable and instantiate a new object to a enoNumber class constructor which initializes the new object.
+        mainENO.EvOdNumber(); // Call the class method EvOdNumber() from mainENO variable. This will run the whole process of enoNumber class.
+    }
 }
---
+++ MSU/OOP/Assignment2/classes/enoNumber.java
@@ -0,0 +1,43 @@
+package MSU.OOP.Assignment2.classes;
+import java.util.Scanner;
+
+public class enoNumber 
+{
+    public void EvOdNumber()
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Type any integer value:");
+        int num = scan.nextInt();
+                
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        if(evenNum(num))
+        {
+            System.out.println(num + " is an even number!");
+        }
+        else
+        {
+            System.out.println(num + " is an odd number!");
+        }   
+        
+        scan.close(); // Close the Scanner class to avoid resource leak warning.
+    }
+
+    public static boolean evenNum(int number)
+    {
+        if(number % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
